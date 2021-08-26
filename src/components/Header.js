705b939b7fb4f2662f2704c734b4ac1a66718d15
github_Praveen-webdev <praveen1598810@gmail.com>
2021-08-26 22:53:13 +0530

    complete header

---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./header.css";
+
+export default function Header() {
+	return (
+		<div className="header">
+			<nav className="navbar  ">
+				<div id="header-items" className="container-fluid">
+					<h4 className="navbar-brand d-flex mt-1 ">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="16"
+							height="16"
+							fill="currentColor"
+							className="bi bi-film"
+							viewBox="0 0 16 16"
+						>
+							<path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm4 0v6h8V1H4zm8 8H4v6h8V9zM1 1v2h2V1H1zm2 3H1v2h2V4zM1 7v2h2V7H1zm2 3H1v2h2v-2zm-2 3v2h2v-2H1zM15 1h-2v2h2V1zm-2 3v2h2V4h-2zm2 3h-2v2h2V7zm-2 3v2h2v-2h-2zm2 3h-2v2h2v-2z" />
+						</svg>
+						Moviepedia
+					</h4>
+					<form className="d-flex">
+						<input
+							className="form-control "
+							type="search"
+							id="input"
+							placeholder="Search movies"
+							aria-label="Search"
+						/>
+						<button type="submit" id="search">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="16"
+								fill="currentColor"
+								class="bi bi-search"
+								viewBox="0 0 16 16"
+							>
+								<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+							</svg>
+						</button>
+					</form>
+				</div>
+			</nav>
+		</div>
+	);
+}
